c02d37a618585acd0bdbe80a9771f0ea57ba3114
Rotus7 <rotus97@icloud.com>
2022-11-16 02:07:44 +0900

    edit 2.py $1-3

--- 2.py
+++ 2.py
@@ -1,12 +1,32 @@
+import numpy as np
+
+
+# 1から128までの整数を内包表記で作成
 arr = [i for i in range(1, 129)]
 
-target = int(input("Please enter id (1 to 128) : ")) - 1
+# 配列のシャッフル
+np.random.shuffle(arr)
 
-tmp = arr[target]
+# STDIN
+s = input("Please enter id (1 to 128) : ")
 
-for j in range(target):
-    arr[target - j] = arr[target - j - 1]
+# str -> int
+select = int(s)
 
+# 選んだ数字の探索　(idx = arr.index() でも可)
+for j in range(len(arr)):
+    if select == arr[j]:
+        break;
+
+# 対象をtmpに代入
+tmp = arr[j]
+
+# 配列の要素数に合わせてselectから-1する
+target = select - 1
+
+# 移動
+for k in range(target):
+    arr[target - k] = arr[target - k - 1]
+
+# 配列の先頭に対象の数字を移動
 arr[0] = tmp
-
-print(arr)
