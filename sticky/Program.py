b92d4a9551455565df41afd6ea62b247c13bfd0c
Rotus7 <rotus97@icloud.com>
2022-11-16 03:03:03 +0900

    divide source file #4

---
+++ sticky/Program.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+from src import Window
+from src import Back
+
+
+def main():
+    formsInstance = Window.Form()
+    formsInstance.FormLoop()
+
+if __name__ == "__main__":
+    main()
---
+++ sticky/src/Back.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+
+class Memo():
+    id = 0
+    text = ""
+    right = 0
+    bottom = 0
+    width = 0
+    height = 0
+
+class MemoListNode():
+    data = Memo()
+    prevNode = None
+    nextNode = None
+
+
+def Format():
+        one = Memo()
+        one.id = id
+        one.right = 80
+        one.bottom = 80
+        one.width = 30
+        one.height = 20
+
+        two = Memo()
+        two.id = id
+        two.right = 100
+        two.bottom = 10
+        two.width = 40
+        two.height = 40
+
+        three = Memo()
+        three.id = id
+        three.right = 50
+        three.bottom = 20
+        three.width = 30
+        three.height = 50
+
+        four = Memo()
+        four.id = id
+        four.right = 20
+        four.bottom = 60
+        four.width = 40
+        four.height = 30
+
+        five = Memo()
+        five.id = id
+        five.right = 90
+        five.bottom = 80
+        five.width = 40
+        five.height = 10
+
