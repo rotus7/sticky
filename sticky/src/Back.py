1336730c1f08c4753d9a3418bca6dea0b81f2de6
Rotus7 <rotus97@icloud.com>
2022-11-16 14:52:51 +0900

    edit #4-3

--- sticky/src/Back.py
+++ sticky/src/Back.py
@@ -1,5 +1,8 @@
 # coding: utf-8
 
+MEMO_MAX_SIZE = 8
+
+
 class Memo():
     id = 0
     text = ""
@@ -9,42 +12,42 @@ class Memo():
     height = 0
 
 class MemoListNode():
-    data = Memo()
+    data = None
     prevNode = None
     nextNode = None
 
 
 def Format():
         one = Memo()
-        one.id = id
+        one.id = 1
         one.right = 80
         one.bottom = 80
         one.width = 30
         one.height = 20
 
         two = Memo()
-        two.id = id
+        two.id = 2
         two.right = 100
         two.bottom = 10
         two.width = 40
         two.height = 40
 
         three = Memo()
-        three.id = id
+        three.id = 3
         three.right = 50
         three.bottom = 20
         three.width = 30
         three.height = 50
 
         four = Memo()
-        four.id = id
+        four.id = 4
         four.right = 20
         four.bottom = 60
         four.width = 40
         four.height = 30
 
         five = Memo()
-        five.id = id
+        five.id = 5
         five.right = 90
         five.bottom = 80
         five.width = 40
