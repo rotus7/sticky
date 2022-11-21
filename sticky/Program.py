fffda87c2cf0b8362c069883b412fac1092d7d48
Rotus7 <rotus97@icloud.com>
2022-11-22 00:29:31 +0900

    start using qt, add icon, change files #5

--- sticky/Program.py
+++ sticky/Program.py
@@ -1,11 +1,26 @@
-# coding: utf-8
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import QIcon
+from src.ui import Ui_Form
 
-from src import Window
+
+class gui(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super(gui, self).__init__(parent)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+    def reset(self):
+        print("Hello")
 
 
 def main():
-    formsInstance = Window.Form()
-    formsInstance.FormLoop()
+    app = QtWidgets.QApplication(sys.argv)
+    app.setWindowIcon(QIcon('image/sticky.ico'))
+    window = gui()
+    window.show()
+    sys.exit(app.exec_())
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
--- sticky/src/ctr.py
+++ sticky/src/ctr.py
@@ -1,9 +1,11 @@
 # coding: utf-8
 
-MEMO_MAX_SIZE = 8
+from func import const, mouse, button
+
+const.MAX_STICKY = 8
 
 
-class Memo():
+class Sticky():
     id = 0
     text = ""
     right = 0
@@ -11,42 +13,37 @@ class Memo():
     width = 0
     height = 0
 
-class MemoListNode():
-    data = None
-    prevNode = None
-    nextNode = None
-
 
 def Format():
-        one = Memo()
+        one = Sticky()
         one.id = 1
         one.right = 80
         one.bottom = 80
         one.width = 30
         one.height = 20
 
-        two = Memo()
+        two = Sticky()
         two.id = 2
         two.right = 100
         two.bottom = 10
         two.width = 40
         two.height = 40
 
-        three = Memo()
+        three = Sticky()
         three.id = 3
         three.right = 50
         three.bottom = 20
         three.width = 30
         three.height = 50
 
-        four = Memo()
+        four = Sticky()
         four.id = 4
         four.right = 20
         four.bottom = 60
         four.width = 40
         four.height = 30
 
-        five = Memo()
+        five = Sticky()
         five.id = 5
         five.right = 90
         five.bottom = 80
